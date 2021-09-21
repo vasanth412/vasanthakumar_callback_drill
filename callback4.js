766ed6b9a97271201b3bbfee05f39378fc0b323c
VASANTHAKUMAR B <vasanthakumar.b.17@mountblue.tech>
2021-09-21 14:37:13 +0530

    Adding question

--- callback4.js
+++ callback4.js
@@ -1,3 +1,10 @@
+// Problem 4: Write a function that will use the previously written functions to get
+// the following information. You do not need to pass control back to the code that called it.
+
+//   Get information from the Thanos boards
+//   Get all the lists for the Thanos board
+//   Get all cards for the Mind list simultaneously.
+
 const boards = require('./data/boards.json');
 const promise1 = require('./callback1');
 const promise2 = require('./callback2');
--- callback6.js
+++ callback6.js
@@ -1,3 +1,9 @@
+// Problem 6: Write a function that will use the previously written functions to get the
+// following information. You do not need to pass control back to the code that called it.
+//     Get information from the Thanos boards
+//     Get all the lists for the Thanos board
+//     Get all cards for all lists simultaneously
+
 const boards = require('./data/boards.json');
 const promise1 = require('./callback1');
 const promise2 = require('./callback2');
